571952c26bcd09d82f690ed52299886fa66a14f0
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-05-08 00:04:29 +0100

    create window

--- easy-convert/src/main/index.ts
+++ easy-convert/src/main/index.ts
@@ -2,3 +2,20 @@
 // aks user where to save the converted files
 // run lame to convert
 // send progress updates to ui
+
+import { BrowserWindow } from "electron";
+import path from "path";
+
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "../preload/index.js"),
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:5173");
+};
